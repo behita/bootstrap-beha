4731e5cfd1c7f011aedef9c2cf891a3a89a312f5
behita <behatriz.13gnicasio@gmail.com>
2016-03-10 15:42:50 -0500

    zapatilla

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
+	<title>GARGATE</title>
 	<link rel="stylesheet" href="public/css/bootstrap.css">
 </head>
 <body>
